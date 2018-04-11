8973cab7e717d4b40baabf63642424ebbe7f9173
Pakapon Kaewmark <sleep191@gmail.com>
2018-04-11 10:23:50 +0700

    Add Home Scene

---
+++ Assets/Scripts/UI/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HomeController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnLoginButtonClicked () {
+		StartCoroutine(LoadAsyncScene("BossSelection"));
+	}
+
+	private IEnumerator LoadAsyncScene(string sceneName) {
+		AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
+
+		while(!asyncLoad.isDone) {
+			yield return null;
+		}
+	}
+}
